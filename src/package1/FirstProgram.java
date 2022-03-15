fd64679fbcae50fa44df456e0a0791eb79d42fd4
Sandhya1505 <sandhya.rathod@tray.com>
2022-03-15 15:52:20 +0530

    New Commit as tester

--- src/package1/FirstProgram.java
+++ src/package1/FirstProgram.java
@@ -5,8 +5,8 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String s1 = "GIT";
-		if(s1=="GIT")
+		String s1 = "GITAAA";
+		if(s1=="GITAAA")
 		{
 			System.out.println("Learning GIT from youtube");
 		}
