a1b2d5805164588f163fb723d2b81fa3356a2b2e
sandhya <sandhya@LAPTOP-FKGOBKRQ>
2022-03-15 12:20:24 +0530

    Commiting SecondProgram as a tester by creating another branch: BRanch-Tester

---
+++ src/package1/SecondProgram.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Sandhya";
+		if(name=="Sandhya")
+		{
+			System.out.println("My Name is Sandhya");
+		}
+		else
+		{
+			System.out.println("My Surname is Rathod");
+		}
+
+	}
+
+}
