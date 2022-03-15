2c41ebe11ef001eb7cb721b68a1aa0de7a64b384
sandhya <sandhya@LAPTOP-FKGOBKRQ>
2022-03-15 12:20:24 +0530

    This is my first commit

---
+++ src/package1/FirstProgram.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "GIT";
+		if(s1=="GIT")
+		{
+			System.out.println("Learning GIT from youtube");
+		}
+		else
+		{
+			System.out.println("Focus on learning");
+		}	
+
+	}
+
+}
